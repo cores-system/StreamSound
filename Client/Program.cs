144ec4b10ff7d35134f76d63b5ec59e7d9b02019
cores-system <dev@sped-mail.ru>
2019-06-04 16:45:00 +0300

    Коменты

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,47 +9,49 @@ namespace Client
     {
         static WaveOutEvent wo;
         static BufferedWaveProvider bwp;
-        static WaveFileWriter waweFile;
 
 
         static void Main(string[] args)
         {
+            // Удаленный WaveIn
             var connection = new HubConnectionBuilder().WithUrl("http://192.168.0.110:5089/sound").Build();
 
+            #region Closed
             connection.Closed += async (error) =>
             {
                 await Task.Delay(10);
                 await connection.StartAsync();
             };
+            #endregion
 
+            #region ConnectionId
             connection.On<string>("ConnectionId", ConnectionId =>
             {
                 Console.WriteLine("Id: " + ConnectionId);
             });
+            #endregion
 
+            #region DataAvailable
             connection.On<byte[], int>("DataAvailable", (Buffer, BytesRecorded) =>
             {
                 bwp.AddSamples(Buffer, 0, BytesRecorded);
             });
+            #endregion
 
+            // Подключаемся
             connection.StartAsync().Wait();
             Console.WriteLine(connection.State.ToString());
 
-
-
-
-
+            #region WaveOut
             wo = new WaveOutEvent();
             bwp = new BufferedWaveProvider(new WaveFormat(48000, 2));
-            //waweFile = new WaveFileWriter(@"D:\Trash\Temporarily\publish\this.wav", new WaveFormat(48000, 2));
             bwp.DiscardOnBufferOverflow = true;
             wo.Init(bwp);
             wo.Play();
+            #endregion
 
 
-
-
-
+            // Wait
             Console.ReadLine();
         }
     }
