79b8c4cf1077408983992aeb0507761696623327
cores-system <dev@sped-mail.ru>
2019-06-05 18:27:29 +0300

    Remove dev

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -131,25 +131,23 @@ namespace Client
             long acceptedPackages = 0;
             hubConnection.On<byte[], int>("DataAvailable", (Buffer, BytesRecorded) =>
             {
-                // Запускаем таймер
+                int offset = 0;
+
+                // Начало воспроизведения звука
                 if (acceptedPackages == 0)
                     watch.Start();
 
-                // Счетчик пакетов
-                acceptedPackages++;
-
-                // 
-                int offset = 0;
-
                 // Пакет пришел слишком поздно, вместо части пакета уже проигран пустой звук
-                if (watch.ElapsedMilliseconds > (acceptedPackages * bufferMilliseconds))
+                if (acceptedPackages > 0 && watch.ElapsedMilliseconds > (acceptedPackages * bufferMilliseconds))
                 {
                     // Разница времени
                     long differenceMs = watch.ElapsedMilliseconds - (acceptedPackages * bufferMilliseconds);
 
                     // Разница больше BufferMilliseconds
-                    if (bufferMilliseconds >= differenceMs)
+                    if (bufferMilliseconds >= differenceMs) {
+                        acceptedPackages++;
                         return;
+                    }
 
                     // Смещаем offset
                     offset = (int)(differenceMs * 192); // 1ms = 192 byte
@@ -165,116 +163,6 @@ namespace Client
                 }
 
                 // Заполняем буфер
-                bwp.AddSamples(Buffer, offset, BytesRecorded);
-            });
-            #endregion
-
-            #region Подключаемся
-            try
-            {
-                await hubConnection.StartAsync();
-                textBox1.Text += hubConnection.State.ToString() + Environment.NewLine;
-            }
-            catch
-            {
-                await HubConnection_Closed(null);
-            }
-            #endregion
-        }
-        #endregion
-
-
-
-
-        #region SoundPlayBack2
-        async void SoundPlayBack2()
-        {
-            int bufferMilliseconds = 20;
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-            #region ConnectionId
-            hubConnection.On<string, int>("ConnectionId", (ConnectionId, BufferMilliseconds) =>
-            {
-                textBox1.Text = "Id: " + ConnectionId + Environment.NewLine;
-                bufferMilliseconds = BufferMilliseconds;
-            });
-            #endregion
-
-            #region DataAvailable
-            int acceptedPackages = 0;
-            hubConnection.On<byte[], int>("DataAvailable", (Buffer, BytesRecorded) =>
-            {
-                if (acceptedPackages > 0)
-                {
-                    int b1 = (acceptedPackages * (bufferMilliseconds * 192)); // 2 пакета за < 40ms = 7 680
-                    int b2 = (int)(watch.ElapsedMilliseconds * 192);          // 40ms = 7 680
-
-                    if (b2 > b1)
-                    {
-                        File.AppendAllText("debug.txt", DateTime.Now.Minute + ":" + DateTime.Now.Second + "" + DateTime.Now.Second + " => " + b1 + " / " + b2 + Environment.NewLine);
-                        bwp.ClearBuffer();
-                        watch.Reset();
-                        acceptedPackages = 0;
-                    }
-                }
-
-                acceptedPackages++;
-                bwp.AddSamples(Buffer, 0, BytesRecorded);
-            });
-            #endregion
-
-            #region Подключаемся
-            try
-            {
-                await hubConnection.StartAsync();
-                textBox1.Text += hubConnection.State.ToString() + Environment.NewLine;
-            }
-            catch
-            {
-                await HubConnection_Closed(null);
-            }
-            #endregion
-        }
-        #endregion
-
-
-
-        #region SoundPlayBack
-        async void SoundPlayBack()
-        {
-            // default: BufferMilliseconds == 20ms
-            int whatPackageToCheck = 3000, maxBufferedBytes = 3840;
-
-            #region ConnectionId
-            hubConnection.On<string, int>("ConnectionId", (ConnectionId, BufferMilliseconds) =>
-            {
-                textBox1.Text = "Id: " + ConnectionId + Environment.NewLine;
-
-                // 10 секунд
-                whatPackageToCheck = (1000 / BufferMilliseconds) * 10;
-
-                // 1ms == 192 byte
-                maxBufferedBytes = BufferMilliseconds * 192;
-            });
-            #endregion
-
-            #region DataAvailable
-            int acceptedPackages = 1;
-            hubConnection.On<byte[], int>("DataAvailable", (Buffer, BytesRecorded) =>
-            {
-                int offset = 0;
-                if ((acceptedPackages % whatPackageToCheck) == 0)
-                {
-                    // Буфер превышает BufferMilliseconds в два раза
-                    if (bwp.BufferedBytes > maxBufferedBytes * 2)
-                        return;
-
-                    // В буфере звука больше установленного BufferMilliseconds
-                    if (bwp.BufferedBytes > maxBufferedBytes)
-                        offset = bwp.BufferedBytes - Buffer.Length;
-                }
-
                 acceptedPackages++;
                 bwp.AddSamples(Buffer, offset, BytesRecorded);
             });
